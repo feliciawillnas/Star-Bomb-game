602e9eb42fb7dfc858a411a1bf2778d20060c777
stenbumling <marcusrosin@outlook.com>
2023-01-19 13:26:25 +0100

    Made bomb bounce between x positions

--- src/bomb.ts
+++ src/bomb.ts
@@ -3,14 +3,15 @@ class Bomb {
     // private color: string;
     private x = 0;
     private y = 0;
+    private vx = 4;
     private diameter = 50;
 
     //CONSTRUCTOR////////////////////////
     constructor(diameter: number){
-        this.x = random((width / 2 - playScene.rectW / 2) + diameter, (width / 2 + playScene.rectW / 2) - diameter),
-        this.y = random((height / 2 - playScene.rectH / 2 + playScene.offsetTop) + diameter, (height / 2 + playScene.rectH / 2 + playScene.offsetTop) - diameter)
-    console.log(this.x)
-    console.log(this.y)
+        this.x = random((width / 2 - playScene.rectW / 2) + diameter / 2,
+        (width / 2 + playScene.rectW / 2) - diameter / 2);
+        this.y = random((height / 2 - playScene.rectH / 2 + playScene.offsetTop) + diameter / 2,
+        (height / 2 + playScene.rectH / 2 + playScene.offsetTop) - diameter / 2);
     }
     //METHODS//////////////////////////
     
@@ -21,6 +22,7 @@ class Bomb {
     //Draw
     public draw(){
         bombs.display();
+        bombs.move();
     }
     // let spring = 0.05;
     // let gravity = 0;
@@ -32,4 +34,12 @@ display() {
     fill(255);
     ellipse(this.x, this.y, this.diameter);
   }
-}
+
+move() {
+    this.x += this.vx
+    if (this.x > (width / 2 + playScene.rectW / 2) - this.diameter / 2) {
+        this.vx =- 4;
+    } else if (this.x < (width / 2 - playScene.rectW / 2) + this.diameter / 2) {
+        this.vx =+ 4;
+    }
+}}
