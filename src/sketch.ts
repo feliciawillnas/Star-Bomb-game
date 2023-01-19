feb2a1a8f28e3e7d119fb08606ad7d0d0d3db276
Linus Hernvall <linus.hernvall@gmail.com>
2023-01-19 10:48:53 +0100

    Added class and new imgs to sketch.

--- src/sketch.ts
+++ src/sketch.ts
@@ -8,6 +8,12 @@ let backgroundImg: p5.Image;
 let galaxGoalImg: p5.Image;
 let playboardBGImg: p5.Image;
 let gameFont: p5.Font;
+let player: Player;
+
+let rocketImgPink1: p5.Image;
+let rocketImgBlue1: p5.Image;
+let rocketImgPink2: p5.Image;
+let rocketImgBlue2: p5.Image;
 /**
  * Built in preload function in P5
  * This is a good place to load assets such as
@@ -15,10 +21,15 @@ let gameFont: p5.Font;
  */
 function preload() {
   // sound: p5.SoundFile = loadSound('../assets/mySound.wav');
-  backgroundImg = loadImage('../assets/images/background.jpg');
-  galaxGoalImg = loadImage('../assets/images/galax.jpg')
-  playboardBGImg = loadImage('../assets/images/playboardBackground.png')
-  gameFont = loadFont('../assets/fonts/PressStart2P-Regular.ttf');
+  backgroundImg = loadImage("../assets/images/background.jpg");
+  galaxGoalImg = loadImage("../assets/images/galax.jpg");
+  playboardBGImg = loadImage("../assets/images/playboardBackground.png");
+  gameFont = loadFont("../assets/fonts/PressStart2P-Regular.ttf");
+
+  rocketImgBlue1 = loadImage("../assets/images/blueRocket1.png");
+  rocketImgPink1 = loadImage("../assets/images/pinkRocket1.png");
+  rocketImgBlue2 = loadImage("../assets/images/bluerocket2.png");
+  rocketImgPink2 = loadImage("../assets/images/pinkrocket2.png");
 }
 
 /**
@@ -30,23 +41,24 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(60);
-  imageMode(CENTER)
-  rectMode(CENTER)
-  textAlign(CENTER)
-  angleMode(DEGREES)
+  imageMode(CENTER);
+  rectMode(CENTER);
+  textAlign(CENTER);
+  angleMode(DEGREES);
   textFont(gameFont);
-  
+
   game = new Game();
   playScene = new PlayScene();
   goal = new Goal();
   scoreInterface = new ScoreInterface();
+  player = new Player();
 }
 
 /**
  * Built in draw function in P5
  * This is a good place to call public methods of the object
  * you created in the setup function above
-*/
+ */
 function draw() {
   game.update();
   game.draw();
