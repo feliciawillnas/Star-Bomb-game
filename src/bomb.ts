6358194c1926753a8ad285358db224a3dc0801d5
stenbumling <marcusrosin@outlook.com>
2023-01-27 09:43:08 +0100

    Made bomb blink faster and faster before detonating

--- src/bomb.ts
+++ src/bomb.ts
@@ -41,11 +41,67 @@ class Bomb {
   // When timer is under 5 seconds, draw a red bomb
   // When timer is under 0.2 seconds, draw an explosion
   private drawBombImage() {
+    // Normal bomb image
     if (this.timeToLive > 5500){
         image(images.neonGreenBombClear, this.x, this.y);
-    } else if (this.timeToLive > 200 && this.timeToLive <= 5500) {
+
+    // Bomb blinks every half second
+    } else if (this.timeToLive <= 5500 && this.timeToLive > 5000) {
         image(images.redBomb, this.x, this.y)
-    } else {
+    } else if (this.timeToLive <= 5000 && this.timeToLive > 4500){
+        image(images.neonGreenBombClear, this.x, this.y);
+    } else if (this.timeToLive <= 4500 && this.timeToLive > 4000) {
+        image(images.redBomb, this.x, this.y)
+    } else if (this.timeToLive <= 4000 && this.timeToLive > 3500){
+        image(images.neonGreenBombClear, this.x, this.y);
+
+
+    // Bomb blinks every 0.25 second
+    } else if (this.timeToLive <= 3500 && this.timeToLive > 3250){
+        image(images.redBomb, this.x, this.y);
+    } else if (this.timeToLive <= 3250 && this.timeToLive > 3000) {
+        image(images.neonGreenBombClear, this.x, this.y)
+    } else if (this.timeToLive <= 3000 && this.timeToLive > 2750){
+        image(images.redBomb, this.x, this.y);
+    } else if (this.timeToLive <= 2750 && this.timeToLive > 2500) {
+        image(images.neonGreenBombClear, this.x, this.y)
+    } else if (this.timeToLive <= 2500 && this.timeToLive > 2250){
+        image(images.redBomb, this.x, this.y);
+    } else if (this.timeToLive <= 2250 && this.timeToLive > 2000){
+        image(images.neonGreenBombClear, this.x, this.y);
+    } else if (this.timeToLive <= 2000 && this.timeToLive > 1750) {
+        image(images.redBomb, this.x, this.y)
+    } else if (this.timeToLive <= 1750 && this.timeToLive > 1500){
+        image(images.neonGreenBombClear, this.x, this.y);
+
+    // Bomb blinks every 0.1 second
+    } else if (this.timeToLive <= 1500 && this.timeToLive > 1400) {
+        image(images.redBomb, this.x, this.y)
+    } else if (this.timeToLive <= 1400 && this.timeToLive > 1300){
+        image(images.neonGreenBombClear, this.x, this.y);
+    } else if (this.timeToLive <= 1300 && this.timeToLive > 1200) {
+        image(images.redBomb, this.x, this.y)
+    } else if (this.timeToLive <= 1200 && this.timeToLive > 1100){
+        image(images.neonGreenBombClear, this.x, this.y);
+    } else if (this.timeToLive <= 1100 && this.timeToLive > 1000) {
+        image(images.redBomb, this.x, this.y)
+    } else if (this.timeToLive <= 1000 && this.timeToLive > 900) {
+        image(images.neonGreenBombClear, this.x, this.y)
+    } else if (this.timeToLive <= 900 && this.timeToLive > 800){
+        image(images.redBomb, this.x, this.y);
+    } else if (this.timeToLive <= 800 && this.timeToLive > 700) {
+        image(images.neonGreenBombClear, this.x, this.y)
+    } else if (this.timeToLive <= 700 && this.timeToLive > 600){
+        image(images.redBomb, this.x, this.y);
+    } else if (this.timeToLive <= 600 && this.timeToLive > 500) {
+        image(images.neonGreenBombClear, this.x, this.y)
+    } else if (this.timeToLive <= 500 && this.timeToLive > 400){
+        image(images.redBomb, this.x, this.y);
+    } else if (this.timeToLive <= 400 && this.timeToLive > 300) {
+        image(images.neonGreenBombClear, this.x, this.y)
+
+    // Draws explosion image
+    } else{
         image(images.explosion, this.x, this.y, 40, 40);
     }
   }
